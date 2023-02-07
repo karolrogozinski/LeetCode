4f4df4cff458b57239ec1792ac4c5606fd426150
Karol <rogo.karol@gmail.com>
2023-02-07 22:33:31 +0100

    Fruits goes brrr

---
+++ MEDIUM/904. Fruit Into Baskets/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+    def totalFruit(self, fruits: list[int]) -> int:
+        max_length = 0
+        curr_length = 0
+        unique = 0
+        start_idx = 0
+        basket = dict()
+
+        for idx in range(len(fruits)):
+            fruit = fruits[idx]
+            if fruit not in basket or not basket[fruit]:
+                basket[fruit] = 1
+                unique += 1
+            else:
+                basket[fruit] += 1
+            
+            if unique <= 2:
+                curr_length += 1
+                if curr_length > max_length:
+                    max_length = curr_length
+                continue
+
+            start_fruit = fruits[start_idx]
+            start_idx += 1
+            basket[start_fruit] -= 1
+            if not basket[start_fruit]:
+                unique -= 1
+
+        return max_length
