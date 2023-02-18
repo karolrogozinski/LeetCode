26ee28114e16a7f96bba4382b9e7f53d6db9d714
Karol Rogoziński <73389492+karolrogozinski@users.noreply.github.com>
2023-02-18 20:54:32 +0100

    Again computer not available

--- Tmp.py
+++ Tmp.py
@@ -14,3 +14,24 @@ Class Solution:
         prev = float(-inf)
         inorder(root)
         return min_diff
+
+
+
+# 226. Invert Binary Tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def invert(node: Optional[TreeNode]) -> None:
+            if node:
+                node.left, node.right = node.right, node.left
+                invert(node.left)
+                invert(node.right)
+
+        invert(root)
+        return root
